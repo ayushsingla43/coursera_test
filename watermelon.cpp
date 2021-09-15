530699db1ce4c24a9dc4b2a5194e40f6a2b3278b
ayushsingla43 <72762726+ayushsingla43@users.noreply.github.com>
2021-09-15 22:42:48 +0530

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int w; cin>>w;
+    if(w%2!=0) cout<<"NO"<<endl;
+    else{
+        if(w==2) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
